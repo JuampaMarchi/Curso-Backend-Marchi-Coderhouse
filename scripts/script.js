e12607f6a684b5e8b9176d1685ff79277845273b
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-11-16 21:01:58 -0300

    Clase Express Avanzado

--- scripts/script.js
+++ scripts/script.js
@@ -2,13 +2,32 @@ const moment = require('moment')
 const express = require('express')
 const app = express()
 const PORT = 777
+const FRASE = 'Hola Viteh'
 
-app.get('/', function(req, res, next){
-    res.send(`Bienvenido al servidor. Hoy es: ${moment().format('DD/MM/YYYY hh:mm:ss')}`)
+app.get('/frase', function(req, res, next){
+    
+    res.json({"frase": FRASE})
+})
+
+app.get('/letras/:num', function(req, res, next){
+    let {num} = req.params
+    let response = null
+    if(Number(num)){
+        let frase = FRASE.split("");
+        let finalNum = num - 1;
+        if(finalNum <= frase.length){
+            response = frase[finalNum]
+        } else {
+            response = 'el parametro esta mal'
+        }
+    } else {
+        response = 'hay algo mal'
+    }
+    res.json({"hola": response})
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Escuchando desde puerto: ${PORT}`)
+    console.log(`Escuchando desde http:/localhost:${PORT}`)
 })
 
 
