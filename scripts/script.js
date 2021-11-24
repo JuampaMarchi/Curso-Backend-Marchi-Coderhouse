3beab6a606dc3b35907591c7a4848c49fff8d04c
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-11-24 14:10:27 -0300

    Ejercicios POST y PUT

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,32 @@ let router = new Router;
 const PORT = 8080
 const products = JSON.parse(fs.readFileSync('./productos.json', 'utf-8'))
 
+//Middlewares
+app.use(express.static("public/html"))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
+
 
 router.get('/:id', (req, res, next)=>{
     const reqId = req.params
     if(reqId.id > 0 && reqId.id <= products.length){
         const item = products.find(e => e.id == reqId.id)
-        res.json(item)
+        return res.json(item)
     }
-    res.send(`El id: ${reqId.id} no existe en nuestra base de datos`)
+    return res.send(`El id: ${reqId.id} no existe en nuestra base de datos`)
+})
+
+router.get('/', (req, res, next)=>{
+    const reqId = req.query
+    if(reqId.id > 0 && reqId.id <= products.length){
+        const item = products.find(e => e.id == reqId.id)
+        return res.json(item)
+    } else {
+        if(reqId.id == undefined){
+            return res.json(products)
+        }
+    }
+    return res.send(`El query id: ${reqId.id} no existe en nuestra base de datos`)
 })
 
 router.post('/', (req, res, next)=>{
@@ -28,19 +44,24 @@ router.post('/', (req, res, next)=>{
 
 router.put('/', (req, res, next)=>{
     const {prod_id, new_price} = req.query
-    console.log('id', prod_id)
-    console.log('new_price', new_price)
     if(prod_id > 0 && prod_id <= products.length){
         const item = products.find(e => e.id == prod_id)
         item.price = new_price
         fs.writeFileSync('./productos.json', JSON.stringify(products))
-        res.send(`El producto ${item.name} fué actualizado. Nuevo precio: $${item.price}`)
+        return res.send(`El producto ${item.name} fué actualizado. Nuevo precio: $${item.price}`)
     }
-    res.send(`Error: el id '${prod_id}' no se corresponde a ningun producto`)
+    return res.send(`Error: el id '${prod_id}' no se corresponde a ningun producto`)
 })
 
-router.get('/', (req, res, next)=>{
-    res.json(products)
+router.delete('/', (req, res, next)=>{
+    const reqProd = req.query
+    if(reqProd.id > 0 && reqProd.id <= products.length){
+        const item = products.find(e => e.id == prod_id)
+        products.splice(products.indexOf(item),1)
+        fs.writeFileSync('./productos.json', JSON.stringify(products))
+        return res.send(`El producto con id ${reqProd.id} fue exitosamente eliminado`)
+    }
+    return res.send(`Error al eliminar producto: el id '${reqProd.id}' no se corresponde a ningun producto`)
 })
 
 app.use('/productos', router)
