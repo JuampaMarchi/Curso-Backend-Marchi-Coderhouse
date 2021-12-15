3b7c985ead398df33f60798327bce26a462d8460
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-12-14 23:49:22 -0300

    prueba en clase

--- utils/socket/index.js
+++ utils/socket/index.js
@@ -1,59 +1,59 @@
-const fs = require('fs')
-const {Server: SocketIO} = require('socket.io')
+// const fs = require('fs')
+// const {Server: SocketIO} = require('socket.io')
 
-class Socket{
-    static instance
-    constructor(http){
-        if(Socket.instance){
-            return Socket.instance
-        }
-        Socket.instance = this
-        this.io = new SocketIO(http)
-        this.chatLog = []
-        this.users = []
-        this.products = JSON.parse(fs.readFileSync('./productos.json', 'utf-8'))
-    }
-    init(){
-        try {
-            this.io.on('connection', socket=>{
-                console.log('Usuario contectado!')
-                socket.emit('init', this.chatLog)
-                socket.on('message', data=>{
-                    this.chatLog.push(data)
-                    this.io.sockets.emit('emitToAll', this.chatLog)
-                })
-                socket.on('addUser', data=>{
-                    console.log('usuario recibido')
-                    const newUser = {
-                        id: socket.id,
-                        ...data,
-                        active: true
-                    }
-                    this.users.push(newUser)
-                    this.io.sockets.emit('loadUsers', this.users)
-                })
-                socket.on('disconnect', data=>{
-                    console.log('Usuario desconectado...')
-                })
-            })
-        } catch (err) {
-            throw new Error(`Ocurrio el siguiente error: ${err}`)
-        }
-    }
-    initProd(){
-        try {
-            this.io.on('connection', socket=>{
-                socket.emit('sendProd', this.products)
-                socket.on('loadProd', data=>{
-                    this.products.push(data)
-                    console.log(this.products)
-                    this.io.sockets.emit('sendToAll', this.products)
-                })
-            })
-        } catch (err) {
-            throw new Error(`Ocurrio el siguiente error: ${err}`)
-        }
-    }
-}
+// class Socket{
+//     static instance
+//     constructor(http){
+//         if(Socket.instance){
+//             return Socket.instance
+//         }
+//         Socket.instance = this
+//         this.io = new SocketIO(http)
+//         this.chatLog = []
+//         this.users = []
+//         this.products = JSON.parse(fs.readFileSync('./productos.json', 'utf-8'))
+//     }
+//     init(){
+//         try {
+//             this.io.on('connection', socket=>{
+//                 console.log('Usuario contectado!')
+//                 socket.emit('init', this.chatLog)
+//                 socket.on('message', data=>{
+//                     this.chatLog.push(data)
+//                     this.io.sockets.emit('emitToAll', this.chatLog)
+//                 })
+//                 socket.on('addUser', data=>{
+//                     console.log('usuario recibido')
+//                     const newUser = {
+//                         id: socket.id,
+//                         ...data,
+//                         active: true
+//                     }
+//                     this.users.push(newUser)
+//                     this.io.sockets.emit('loadUsers', this.users)
+//                 })
+//                 socket.on('disconnect', data=>{
+//                     console.log('Usuario desconectado...')
+//                 })
+//             })
+//         } catch (err) {
+//             throw new Error(`Ocurrio el siguiente error: ${err}`)
+//         }
+//     }
+//     initProd(){
+//         try {
+//             this.io.on('connection', socket=>{
+//                 socket.emit('sendProd', this.products)
+//                 socket.on('loadProd', data=>{
+//                     this.products.push(data)
+//                     console.log(this.products)
+//                     this.io.sockets.emit('sendToAll', this.products)
+//                 })
+//             })
+//         } catch (err) {
+//             throw new Error(`Ocurrio el siguiente error: ${err}`)
+//         }
+//     }
+// }
 
-module.exports = Socket;
+// module.exports = Socket;
