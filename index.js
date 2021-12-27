a0f04383d35eef14c12575cfb8c19e674269975f
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2021-12-27 00:15:04 -0300

    se borro funcion de index principal

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 import { Server } from './server/server.js'
-import { clearChat } from './components/container/controllers/messages.js';
-import moment from 'moment';
-const stamp = moment().format('DD/MM/YYYY HH:mm:ss')
 
 const server = new Server();
 
 server.webSocket()
-
-clearChat()
-
-
-
