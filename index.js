43feee8b58d812929f9583174c1709d3978fec2e
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2022-01-26 21:46:56 -0300

    log in con twitter

--- index.js
+++ index.js
@@ -1,12 +1,117 @@
-import express from "express";
+const express = require('express');
+let expressSession = require("express-session");
+const app = express();
+const PORT = 3000;
+let path = require("path");
+let passport = require("passport");
+let twitterStrategy = require('passport-twitter').Strategy
+let usuarios = [];
+let TWITTER_API_KEY = 'lVADDjTypKsvcr9o5MWAcdMHF'
+let TWITTER_API_KEY_SECRET = 'WGa9XLIml2yb6UuVcBdaj9K3S57U5x9fBPgdCnNFSm19uNrWVn'
 
-const app = express()
-const PORT = 8080
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + "/public"));
 
-app.listen(PORT, ()=>{
-    console.log(`Escuchando desde http://localhost:${PORT}`)
-}) 
+app.set("views", path.join(__dirname, 'views', 'ejs'))
+app.set('view engine', 'ejs');
+
+passport.use(new twitterStrategy({
+    consumerKey: TWITTER_API_KEY,
+    consumerSecret: TWITTER_API_KEY_SECRET,
+    callbackUrl: '/auth/twitter/callback'
+
+}, (token, tokenSecret, profile, done) => {
+    console.log(profile)
+    done(null, profile)
+}
+));
+
+passport.serializeUser((user, done)=>{
+    done(null, user);
+})
+
+passport.deserializeUser((obj, done)=>{
+    done(null, obj);
+})
+
+app.use(expressSession({
+    secret: "secret123",
+    cookie:{
+        httpOnly: false,
+        secure: false,
+        maxAge: 60000
+    },
+    resave:false,
+    saveUninitialized:false
+}))
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+let isAuth = (req, res, next) =>{
+    if(req.isAuthenticated()){
+       return next();
+    }
+    res.redirect('/login');
+}
+
+let isNotAuth = (req, res, next) =>{
+    if(!req.isAuthenticated()){
+        next();  
+    }else{        
+        res.redirect('/datos');
+    }
+}
+
+app.get('/registro', isNotAuth, (req, res, next)=>{
+    res.render('registro');
+})
+
+app.post('/registro', passport.authenticate('register', {failureRedirect: 'registro-error', successRedirect:'datos'}));
+
+app.get('/', (req, res, next)=>{
+    res.redirect('login');
+})
+
+app.get('/login',  (req, res, next)=>{
+    res.render('login');
+})
+
+app.post('/login', passport.authenticate('login', {failureRedirect: 'registro', successRedirect:'datos'}));
+
+
+app.get('/datos', isAuth, (req, res, next)=>{
+    if(!req.user.contador){
+        req.user.contador = 1
+    }else{        
+        req.user.contador++;
+    }
+    res.render('datos', {
+        contador: req.user.contador,
+        usuario: req.user
+    });
+})
+
+app.get('/logout', (req, res, next)=>{
+    req.session.destroy( err =>{
+        if(err) res.send(JSON.stringify(err));
+        res.redirect('login');
+    })
+})
+
+app.get('/auth/twitter', passport.authenticate('twitter'));
+
+app.get('/auth/twitter/callback', passport.authenticate('twitter', {
+    failureRedirect: 'registro', successRedirect: 'datos'
+}));
+
+app.listen(PORT, (err) => {
+    if (err) return console.log('error en listen server', err);
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
