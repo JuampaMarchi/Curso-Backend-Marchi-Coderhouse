e5e0afd55b6b5b2d33ae1cfbaab867d75a0678f8
Juan Pablo Marchi <juanpablomarchi@gmail.com>
2022-02-07 21:33:34 -0300

    archivos clase

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express')
 
-const app = express()
-const PORT = 8080
+let { config } = require("./config");
+let express = require("express");
+let app = express();
+const PORT = config.port;
 
-
-//Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.get("/", (req, res, next)=>{
+    res.send(`Mi fondo es de color ${config.fondo} y el frente d emi casa es de color ${config.frente}`);
+})
 
 app.listen(PORT, ()=>{
-    console.log(`Escuchando desde http://localhost:${PORT}`)
-}) 
+    console.log(`Server On! http://localhost:${PORT}`)
+})
